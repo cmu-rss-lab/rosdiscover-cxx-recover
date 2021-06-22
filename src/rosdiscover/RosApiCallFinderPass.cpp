02436088ecc7800ff6c7b17ac2b4c1567fe6b158
ChrisTimperley <christimperley@googlemail.com>
2021-06-22 16:53:12 -0400

    added cmake boilerplate

---
+++ src/rosdiscover/RosApiCallFinderPass.cpp
@@ -0,0 +1,50 @@
+/**
+ * This pass finds all ROS API calls within a given LLVM module.
+ */
+
+#include <llvm/IR/Function.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/Pass.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/Transforms/IPO/PassManagerBuilder.h>
+
+using namespace llvm;
+
+namespace {
+
+struct RosApiCallFinderPass : llvm::FunctionPass {
+  static char ID;
+
+  RosApiCallFinderPass() : FunctionPass(ID) {}
+
+  void getAnalysisUsage(llvm::AnalysisUsage &info) const override {
+    info.setPreservesAll();
+  }
+
+  bool runOnFunction(llvm::Function &function) override {
+    llvm::outs() << "checking: " << function.getName() << "\n";
+
+    return false;
+  }
+
+}; // RosApiCallFinderPass
+
+
+}
+
+char RosApiCallFinderPass::ID = 0;
+
+static llvm::RegisterPass<RosApiCallFinderPass> X(
+  "find-ros-api-calls",
+  "ROS API Call Finder",
+  false,
+  true
+);
+
+// ensure that the pass runs before any optimisations are applied
+static llvm::RegisterStandardPasses Y(
+  llvm::PassManagerBuilder::EP_EarlyAsPossible,
+  [](const llvm::PassManagerBuilder &Builder, llvm::legacy::PassManagerBase &PM) {
+    PM.add(new RosApiCallFinderPass());
+  }
+);
