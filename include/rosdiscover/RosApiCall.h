0110edc4f07ac59544afd4277cd6501aa328fe5b
ChrisTimperley <christimperley@googlemail.com>
2021-06-25 21:11:09 -0400

    added include directory

---
+++ include/rosdiscover/RosApiCall.h
@@ -0,0 +1,18 @@
+namespace rosdiscover {
+
+class RosApiCall { }; // RosApiCall
+
+
+class RosInitCall : public RosApiCall {
+public:
+  RosInitCall(llvm::Instruction *instruction) : instruction(instruction) { }
+
+  llvm::Instruction getInstruction() const {
+    return instruction;
+  }
+private:
+  llvm::Instruction *instruction;
+}; // RosInitCall
+
+
+} // rosdiscover
