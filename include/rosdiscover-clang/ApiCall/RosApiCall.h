5750efae8d95e512ab0cc4c63aa862e504708015
ChrisTimperley <christimperley@googlemail.com>
2021-06-27 12:51:21 -0400

    switched to const clang::CallExpr

--- include/rosdiscover-clang/ApiCall/RosApiCall.h
+++ include/rosdiscover-clang/ApiCall/RosApiCall.h
@@ -11,26 +11,34 @@ namespace api_call {
 
 class RosApiCall {
 public:
-  clang::CallExpr* getCallExpr() const { return call; }
+  clang::CallExpr const * getCallExpr() const { return call; }
+
+protected:
+  RosApiCall(clang::CallExpr const *call) : call(call) {}
 
 private:
-  RosApiCall(clang::CallExpr *call) : call(call) {}
-
-  clang::CallExpr *call;
+  clang::CallExpr const *call;
 }; // RosApiCall
 
 
 // ros::init
 class RosInitCall : public RosApiCall {
 public:
-  RosInitCall(clang::CallExpr *call) : RosApiCall(call) {}
+  // {
+  //   using namespace clang::ast_matchers;
+  //   static constexpr clang::ast_matchers::StatementMatcher PATTERN = callExpr(
+  //     callee(functionDecl(hasName("ros::init")))
+  //   ).bind("call");
+  // }
 
-  class Finder : public MatchFinder::MatchCallback {
-  public:
-    virtual void run(const MatchFinder::MatchResult &result) override;
-  };
+  RosInitCall(clang::CallExpr const *call) : RosApiCall(call) {}
 
-  static void addFinder(RosApiCallFinder &finder);
+  // class Finder : public MatchFinder::MatchCallback {
+  // public:
+  //   virtual void run(const MatchFinder::MatchResult &result) override;
+  // };
+
+  // static void addFinder(RosApiCallFinder &finder);
 
   // hasKnownName() -> bool
   // getNameExpr() -> Expr
@@ -40,42 +48,42 @@ public:
 // ros::NodeHandle::advertise
 class AdvertiseTopicCall : public RosApiCall {
 public:
-  AdvertiseTopicCall(clang::CallExpr *call) : RosApiCall(call) {}
+  AdvertiseTopicCall(clang::CallExpr const *call) : RosApiCall(call) {}
 };
 
 
 // ros::NodeHandle::subscribe
 class SubscribeTopicCall : public RosApiCall {
 public:
-  SubscribeTopicCall(clang::CallExpr *call) : RosApiCall(call) {}
+  SubscribeTopicCall(clang::CallExpr const *call) : RosApiCall(call) {}
 };
 
 
 // ros::service::call
 class BareServiceCall : public RosApiCall {
 public:
-  BareServiceCall(clang::CallExpr *call) : RosApiCall(call) {}
+  BareServiceCall(clang::CallExpr const *call) : RosApiCall(call) {}
 };
 
 
 // ros::NodeHandle::serviceClient
 class ServiceClientCall : public RosApiCall {
 public:
-  ServiceClientCall(clang::CallExpr *call) : RosApiCall(call) {}
-}:
+  ServiceClientCall(clang::CallExpr const *call) : RosApiCall(call) {}
+};
 
 
 // ros::NodeHandle::advertiseService
 class AdvertiseServiceCall : public RosApiCall {
 public:
-  AdvertiseServiceCall(clang::CallExpr *call) : RosApiCall(call) {}
+  AdvertiseServiceCall(clang::CallExpr const *call) : RosApiCall(call) {}
 };
 
 
 // ros::NodeHandle::getParam
 class GetParamCall : public RosApiCall {
 public:
-  GetParamCall(clang::CallExpr *call) : RosApiCall(call) {}
+  GetParamCall(clang::CallExpr const *call) : RosApiCall(call) {}
 };
 
 
@@ -83,21 +91,21 @@ public:
 // ros::NodeHandle::param
 class GetParamWithDefaultCall : public RosApiCall {
 public:
-  GetParamWithDefaultCall(clang::CallExpr *call) : RosApiCall(call) {}
+  GetParamWithDefaultCall(clang::CallExpr const *call) : RosApiCall(call) {}
 };
 
 
 // ros::param::get
 class BareGetParamCall : public RosApiCall {
 public:
-  BareGetParamCall(clang::CallExpr *call) : RosApiCall(call) {}
+  BareGetParamCall(clang::CallExpr const *call) : RosApiCall(call) {}
 };
 
 
 /// ros::param::param
 class BareGetParamWithDefaultCall : public RosApiCall {
 public:
-  BareGetParamWithDefaultCall(clang::CallExpr *call) : RosApiCall(call) {}
+  BareGetParamWithDefaultCall(clang::CallExpr const *call) : RosApiCall(call) {}
 };
 
 
@@ -105,7 +113,7 @@ public:
 // ros::param::getCached
 class BareGetParamCachedCall : public RosApiCall {
 public:
-  BareGetParamCachedCall(clang::CallExpr *call) : RosApiCall(call) {}
+  BareGetParamCachedCall(clang::CallExpr const *call) : RosApiCall(call) {}
 };
 
 
@@ -114,7 +122,7 @@ public:
 // ros::NodeHandle::getCached
 class GetParamCachedCall : public RosApiCall {
 public:
-  GetParamCachedCall(clang::CallExpr *call) : RosApiCall(call) {}
+  GetParamCachedCall(clang::CallExpr const *call) : RosApiCall(call) {}
 };
 
 
@@ -122,42 +130,42 @@ public:
 // ros::NodeHandle::setParam
 class SetParamCall : public RosApiCall {
 public:
-  SetParamCall(clang::CallExpr *call) : RosApiCall(call) {}
+  SetParamCall(clang::CallExpr const *call) : RosApiCall(call) {}
 };
 
 
 // ros::param::set
 class BareSetParamCall : public RosApiCall {
 public:
-  BareSetParamCall(clang::CallExpr *call) : RosApiCall(call) {}
+  BareSetParamCall(clang::CallExpr const *call) : RosApiCall(call) {}
 };
 
 
 // ros::NodeHandle::hasParam
 class HasParamCall : public RosApiCall {
 public:
-  HasParamCall(clang::CallExpr *call) : RosApiCall(call) {}
+  HasParamCall(clang::CallExpr const *call) : RosApiCall(call) {}
 };
 
 
 // ros::param::has
 class BareHasParamCall : public RosApiCall {
 public:
-  BareHasParamCall(clang::CallExpr *call) : RosApiCall(call) {}
+  BareHasParamCall(clang::CallExpr const *call) : RosApiCall(call) {}
 };
 
 
 // ros::NodeParam::deleteParam
 class DeleteParamCall : public RosApiCall {
 public:
-  DeleteParamCall(clang::CallExpr *call) : RosApiCall(call) {}
+  DeleteParamCall(clang::CallExpr const *call) : RosApiCall(call) {}
 };
 
 
 // ros::param::del
 class BareDeleteParamCall : public RosApiCall {
 public:
-  BareDeleteParamCall(clang::CallExpr *call) : RosApiCall(call) {}
+  BareDeleteParamCall(clang::CallExpr const *call) : RosApiCall(call) {}
 };
 
 
