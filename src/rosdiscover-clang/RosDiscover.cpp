ee5eeeb4d36b0d26c4e7b557a9f484b3ba76c8ab
ChrisTimperley <christimperley@googlemail.com>
2021-06-26 21:15:23 -0400

    added clang tooling boilerplate

--- src/rosdiscover-clang/RosDiscover.cpp
+++ src/rosdiscover-clang/RosDiscover.cpp
@@ -1,3 +1,30 @@
-int main(int argc, char **argv) {
-  return 0;
+#include <clang/Frontend/CompilerInstance.h>
+#include <clang/Frontend/FrontendAction.h>
+#include <clang/Frontend/FrontendActions.h>
+#include <clang/Tooling/Tooling.h>
+#include <clang/Tooling/CommonOptionsParser.h>
+
+#include <clang/ASTMatchers/ASTMatchers.h>
+#include <clang/ASTMatchers/ASTMatchFinder.h>
+
+using namespace clang;
+using namespace clang::ast_matchers;
+using namespace clang::tooling;
+
+static llvm::cl::OptionCategory MyToolCategory("rosdiscover options");
+static llvm::cl::extrahelp CommonHelp(clang::tooling::CommonOptionsParser::HelpMessage);
+
+
+int main(int argc, const char **argv) {
+  CommonOptionsParser optionsParser(argc, argv, MyToolCategory);
+
+  // TODO avoid the need to run things via ClangTool
+  ClangTool tool(optionsParser.getCompilations(), optionsParser.getSourcePathList());
+
+  MatchFinder finder;
+
+  // TODO use MatchFinder::matchAST(ASTContext &)
+
+  auto res = tool.run(newFrontendActionFactory(&finder).get());
+  return res;
 }
