bc28a870c171488bc3b6d8e771cc6fb3902f020b
ChrisTimperley <christimperley@googlemail.com>
2021-06-28 13:12:13 -0400

    integrated API finder

--- src/rosdiscover-clang/RosDiscover.cpp
+++ src/rosdiscover-clang/RosDiscover.cpp
@@ -7,6 +7,9 @@
 #include <clang/ASTMatchers/ASTMatchers.h>
 #include <clang/ASTMatchers/ASTMatchFinder.h>
 
+#include <rosdiscover-clang/ApiCall/RosApiCall.h>
+#include <rosdiscover-clang/ApiCall/Finder.h>
+
 using namespace clang;
 using namespace clang::ast_matchers;
 using namespace clang::tooling;
@@ -14,41 +17,17 @@ using namespace clang::tooling;
 static llvm::cl::OptionCategory MyToolCategory("rosdiscover options");
 static llvm::cl::extrahelp CommonHelp(clang::tooling::CommonOptionsParser::HelpMessage);
 
-StatementMatcher InitMatcher = callExpr(
-  isExpansionInMainFile(),
-  callee(cxxMethodDecl(hasName("init")))
-).bind("init");
-
-
-class ApiCallFinder : public MatchFinder::MatchCallback
-{
-public:
-
-  virtual void run(const MatchFinder::MatchResult &result) override {
-    auto *call = result.Nodes.getNodeAs<clang::CallExpr>("init");
-    llvm::outs() << "found ros::init call: ";
-    call->dumpColor();
-    llvm::outs() << "\n";
-  }
-
-}; // ApiCallFinder
-
-
 int main(int argc, const char **argv) {
   CommonOptionsParser optionsParser(argc, argv, MyToolCategory);
 
   // TODO avoid the need to run things via ClangTool
   ClangTool tool(optionsParser.getCompilations(), optionsParser.getSourcePathList());
 
-  MatchFinder finder;
+  auto calls = rosdiscover::api_call::RosApiCallFinder::find(tool);
+  for (auto *call : calls) {
+    call->getCallExpr()->dumpColor();
+    llvm::outs() << "\n\n";
+  }
 
-  // TODO use an anonymous function?
-  auto init_matcher = ApiCallFinder();
-  finder.addMatcher(InitMatcher, &init_matcher);
-
-  // TODO use MatchFinder::matchAST(ASTContext &)
-
-  llvm::outs() << "finding ROS API calls...\n";
-  auto res = tool.run(newFrontendActionFactory(&finder).get());
-  return res;
+  return 0;
 }
