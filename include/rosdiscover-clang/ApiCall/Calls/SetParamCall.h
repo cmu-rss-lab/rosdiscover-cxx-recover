a5ec7eb6c408591fd81524632f2869db60205b97
ChrisTimperley <christimperley@googlemail.com>
2021-06-30 13:44:48 -0400

    added SetParamCall

--- include/rosdiscover-clang/ApiCall/Calls/SetParamCall.h
+++ include/rosdiscover-clang/ApiCall/Calls/SetParamCall.h
@@ -5,12 +5,28 @@
 namespace rosdiscover {
 namespace api_call {
 
-// ros::NodeHandle::setParam
-class SetParamCall : public RosApiCall {
+class SetParamCall : public NodeHandleRosApiCall {
 public:
   SetParamCall(clang::CallExpr const *call, clang::ASTContext const *context)
-    : RosApiCall(call, context)
+    : NodeHandleRosApiCall(call, context)
   {}
+
+  class Finder : public RosApiCall::Finder {
+  public:
+    Finder(std::vector<RosApiCall*> &found) : RosApiCall::Finder(found) {}
+
+    const clang::ast_matchers::StatementMatcher getPattern() override {
+      using namespace clang::ast_matchers;
+      return callExpr(
+        callee(cxxMethodDecl(hasName("setParam"), ofClass(hasName("ros::NodeHandle"))))
+      ).bind("call");
+    }
+
+  protected:
+    RosApiCall* build(clang::ast_matchers::MatchFinder::MatchResult const &result) override {
+      return new SetParamCall(result.Nodes.getNodeAs<clang::CallExpr>("call"), result.Context);
+    }
+  };
 };
 
 } // rosdiscover::api_call
