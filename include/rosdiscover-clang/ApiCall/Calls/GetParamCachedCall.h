7f94142b156b6b84500392b2a13ea4648e0a09b9
ChrisTimperley <christimperley@googlemail.com>
2021-06-30 13:48:56 -0400

    added GetParamCachedCall

--- include/rosdiscover-clang/ApiCall/Calls/GetParamCachedCall.h
+++ include/rosdiscover-clang/ApiCall/Calls/GetParamCachedCall.h
@@ -7,11 +7,28 @@ namespace api_call {
 
 // ros::NodeHandle::getParamCached
 // ros::NodeHandle::getCached
-class GetParamCachedCall : public RosApiCall {
+class GetParamCachedCall : public NodeHandleRosApiCall {
 public:
   GetParamCachedCall(clang::CallExpr const *call, clang::ASTContext const *context)
-    : RosApiCall(call, context)
+    : NodeHandleRosApiCall(call, context)
   {}
+
+  class Finder : public RosApiCall::Finder {
+  public:
+    Finder(std::vector<RosApiCall*> &found) : RosApiCall::Finder(found) {}
+
+    const clang::ast_matchers::StatementMatcher getPattern() override {
+      using namespace clang::ast_matchers;
+      return callExpr(
+        callee(cxxMethodDecl(anyOf(hasName("getParamCached"), hasName("getCached")), ofClass(hasName("ros::NodeHandle"))))
+      ).bind("call");
+    }
+
+  protected:
+    RosApiCall* build(clang::ast_matchers::MatchFinder::MatchResult const &result) override {
+      return new GetParamCachedCall(result.Nodes.getNodeAs<clang::CallExpr>("call"), result.Context);
+    }
+  };
 };
 
 } // rosdiscover::api_call
--- include/rosdiscover-clang/ApiCall/Calls/GetParamCall.h
+++ include/rosdiscover-clang/ApiCall/Calls/GetParamCall.h
@@ -5,12 +5,28 @@
 namespace rosdiscover {
 namespace api_call {
 
-// ros::NodeHandle::getParam
-class GetParamCall : public RosApiCall {
+class GetParamCall : public NodeHandleRosApiCall {
 public:
   GetParamCall(clang::CallExpr const *call, clang::ASTContext const *context)
-    : RosApiCall(call, context)
+    : NodeHandleRosApiCall(call, context)
   {}
+
+  class Finder : public RosApiCall::Finder {
+  public:
+    Finder(std::vector<RosApiCall*> &found) : RosApiCall::Finder(found) {}
+
+    const clang::ast_matchers::StatementMatcher getPattern() override {
+      using namespace clang::ast_matchers;
+      return callExpr(
+        callee(cxxMethodDecl(hasName("getParam"), ofClass(hasName("ros::NodeHandle"))))
+      ).bind("call");
+    }
+
+  protected:
+    RosApiCall* build(clang::ast_matchers::MatchFinder::MatchResult const &result) override {
+      return new GetParamCall(result.Nodes.getNodeAs<clang::CallExpr>("call"), result.Context);
+    }
+  };
 };
 
 } // rosdiscover::api_call
