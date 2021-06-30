0d823214f236f5d3dfcc55739b5dbc3677013af4
ChrisTimperley <christimperley@googlemail.com>
2021-06-30 13:45:56 -0400

    added SubscribeTopicCall

--- include/rosdiscover-clang/ApiCall/Calls/SubscribeTopicCall.h
+++ include/rosdiscover-clang/ApiCall/Calls/SubscribeTopicCall.h
@@ -5,13 +5,28 @@
 namespace rosdiscover {
 namespace api_call {
 
-// ros::NodeHandle::subscribe
-//
-class SubscribeTopicCall : public RosApiCall {
+class SubscribeTopicCall : public NodeHandleRosApiCall {
 public:
   SubscribeTopicCall(clang::CallExpr const *call, clang::ASTContext const *context)
-    : RosApiCall(call, context)
+    : NodeHandleRosApiCall(call, context)
   {}
+
+  class Finder : public RosApiCall::Finder {
+  public:
+    Finder(std::vector<RosApiCall*> &found) : RosApiCall::Finder(found) {}
+
+    const clang::ast_matchers::StatementMatcher getPattern() override {
+      using namespace clang::ast_matchers;
+      return callExpr(
+        callee(cxxMethodDecl(hasName("subscribe"), ofClass(hasName("ros::NodeHandle"))))
+      ).bind("call");
+    }
+
+  protected:
+    RosApiCall* build(clang::ast_matchers::MatchFinder::MatchResult const &result) override {
+      return new SubscribeTopicCall(result.Nodes.getNodeAs<clang::CallExpr>("call"), result.Context);
+    }
+  };
 };
 
 } // rosdiscover::api_call
