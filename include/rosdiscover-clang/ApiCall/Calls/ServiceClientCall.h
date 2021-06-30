5d3b356bd2ef912556b15de7c3ca68684eb93394
ChrisTimperley <christimperley@googlemail.com>
2021-06-30 13:43:12 -0400

    added ServiceClientCall

--- include/rosdiscover-clang/ApiCall/Calls/ServiceClientCall.h
+++ include/rosdiscover-clang/ApiCall/Calls/ServiceClientCall.h
@@ -5,12 +5,28 @@
 namespace rosdiscover {
 namespace api_call {
 
-// ros::NodeHandle::serviceClient
-class ServiceClientCall : public RosApiCall {
+class ServiceClientCall : public NodeHandleRosApiCall {
 public:
   ServiceClientCall(clang::CallExpr const *call, clang::ASTContext const *context)
-    : RosApiCall(call, context)
+    : NodeHandleRosApiCall(call, context)
   {}
+
+  class Finder : public RosApiCall::Finder {
+  public:
+    Finder(std::vector<RosApiCall*> &found) : RosApiCall::Finder(found) {}
+
+    const clang::ast_matchers::StatementMatcher getPattern() override {
+      using namespace clang::ast_matchers;
+      return callExpr(
+        callee(cxxMethodDecl(hasName("serviceClient"), ofClass(hasName("ros::NodeHandle"))))
+      ).bind("call");
+    }
+
+  protected:
+    RosApiCall* build(clang::ast_matchers::MatchFinder::MatchResult const &result) override {
+      return new ServiceClientCall(result.Nodes.getNodeAs<clang::CallExpr>("call"), result.Context);
+    }
+  };
 };
 
 } // rosdiscover::api_call
