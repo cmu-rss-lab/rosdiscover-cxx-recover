d2972cd8dd2babd967e01588a1b9899b0d3956e8
ChrisTimperley <christimperley@googlemail.com>
2021-06-30 20:28:06 -0400

    added symbolize method

--- include/rosdiscover-clang/ApiCall/NameSymbolizer.h
+++ include/rosdiscover-clang/ApiCall/NameSymbolizer.h
@@ -1,10 +1,14 @@
 #pragma once
 
+#include <clang/AST/Expr.h>
+
 namespace rosdiscover {
 
 /** Attempts to produce a simple symbolic string expression from a ROS name expr **/
 
 class NameSymbolizer {
+public:
+  void symbolize(clang::Expr const *nameExpr) const;
 
 }; // rosdiscover::NameSymbolizer
 
