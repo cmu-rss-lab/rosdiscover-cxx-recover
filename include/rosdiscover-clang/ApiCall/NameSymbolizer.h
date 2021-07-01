b37ca3bc98a6874f13727c0a65cf61fbc06b9239
ChrisTimperley <christimperley@googlemail.com>
2021-06-30 20:26:30 -0400

    added NameSymbolizer

---
+++ include/rosdiscover-clang/ApiCall/NameSymbolizer.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace rosdiscover {
+
+/** Attempts to produce a simple symbolic string expression from a ROS name expr **/
+
+class NameSymbolizer {
+
+}; // rosdiscover::NameSymbolizer
+
+} // rosdiscover
