e3d3c9de7093339e01b2c19828848f97a2767551
ChrisTimperley <christimperley@googlemail.com>
2021-06-30 20:55:11 -0400

    added more exprs

--- include/rosdiscover-clang/ApiCall/NameSymbolizer.h
+++ include/rosdiscover-clang/ApiCall/NameSymbolizer.h
@@ -5,6 +5,32 @@
 namespace rosdiscover {
 
 /** Attempts to produce a simple symbolic string expression from a ROS name expr **/
+class NameExpr {};
+
+class StringLiteral : public NameExpr {
+public:
+  StringLiteral(std::string const literal) : literal(literal) {}
+  std::string const literal;
+};
+
+class RosApiCallExpr : public NameExpr {
+  RosApiCallExpr(api_call::RosApiCall *call) : call(call) {}
+
+  api_call::RosApiCall* getCall() const { return call; }
+
+private:
+  api_call::RosApiCall *call;
+};
+
+class FormalExpr : public NameExpr {
+public:
+  FormalExpr(clang::ParmValDecl const *parameter) : parameter(parameter) {}
+
+ clang::ParmValDecl const * getParameter() const { return parameter; }
+
+private:
+  clang::ParmValDecl const *parameter;
+};
 
 class NameSymbolizer {
 public:
