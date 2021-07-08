94c82856df7de1f0def82d8ff73ff9bf60c4968d
ChrisTimperley <christimperley@googlemail.com>
2021-07-07 22:49:53 -0400

    sketching out summaries some more

--- include/rosdiscover-clang/Summary/FunctionSummary.h
+++ include/rosdiscover-clang/Summary/FunctionSummary.h
@@ -3,9 +3,16 @@
 namespace rosdiscover {
 namespace summary {
 
+// NOTE
+// - we don't need to summarize every possible function
+// - we only need to visit the transitive closure of functions
+//   that call the ROS API
+// - we can traverse the call graph to determine which functions to summarize
+
 class CallArgument {
 
 };
+
 
 // - extract each call, the symbolic arguments to that call, and its path condition
 //  - a subset of those calls represent ROS API calls
@@ -18,14 +25,17 @@ private:
   BoolExpr pathCondition;
 };
 
+
 // we need to state the symbolic variable type for each function parameter that we model
 class FunctionParameter {
 
 };
 
 
+// this is an unresolved summary
 class FunctionSummary {
-
+private:
+  std::vector<ConditionalCall> calls;
 };
 
 
