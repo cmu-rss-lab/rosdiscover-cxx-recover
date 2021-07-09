a44d5cae0c3294c438d7323e75a76c1e53fa2a02
ChrisTimperley <christimperley@googlemail.com>
2021-07-08 22:37:04 -0400

    added summary builder boilerplate

---
+++ include/rosdiscover-clang/Summary/Action.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <clang/Frontend/CompilerInstance.h>
+#include <clang/Frontend/FrontendAction.h>
+#include <clang/Frontend/FrontendActions.h>
+#include <clang/Tooling/Tooling.h>
+
+namespace rosdiscover {
+namespace summary {
+
+
+class SummaryBuilderASTConsumer : public clang::ASTConsumer {
+public:
+  void HandleTranslationUnit(clang::ASTContext &context) override {
+    llvm::outs() << "building summaries\n";
+  }
+};
+
+
+class SummaryBuilderAction : public clang::ASTFrontendAction {
+public:
+  // TODO use pass-by-reference to store summaries
+  static std::unique_ptr<clang::tooling::FrontendActionFactory> factory() {
+    class Factory : public clang::tooling::FrontendActionFactory {
+    public:
+      std::unique_ptr<clang::FrontendAction> create() override {
+        llvm::outs() << "building action...\n";
+        return std::unique_ptr<clang::FrontendAction>(new SummaryBuilderAction());
+      }
+    };
+
+    return std::unique_ptr<clang::tooling::FrontendActionFactory>(new Factory());
+  }
+
+  std::unique_ptr<clang::ASTConsumer> CreateASTConsumer (
+    clang::CompilerInstance &compiler,
+    llvm::StringRef filename
+  ) override {
+    llvm::outs() << "building consumer...\n";
+    return std::unique_ptr<clang::ASTConsumer>(
+      new SummaryBuilderASTConsumer()
+    );
+  }
+};
+
+
+} // rosdiscover::sumary
+} // rosdiscover
--- src/rosdiscover-clang/RosDiscover.cpp
+++ src/rosdiscover-clang/RosDiscover.cpp
@@ -11,6 +11,7 @@
 #include <rosdiscover-clang/ApiCall/RosApiCall.h>
 #include <rosdiscover-clang/ApiCall/Finder.h>
 #include <rosdiscover-clang/CallExprFinder.h>
+#include <rosdiscover-clang/Summary/Action.h>
 
 using namespace clang;
 using namespace clang::ast_matchers;
@@ -33,13 +34,21 @@ int main(int argc, const char **argv) {
   // clang::SourceManager sourceManager(diagnostics, tool.getFiles());
 
   // TODO build the call graph
-  rosdiscover::CallExprFinder::find(tool);
+  // rosdiscover::CallExprFinder::find(tool);
 
+  // build some function summaries
+  auto action = rosdiscover::summary::SummaryBuilderAction::factory();
+  tool.run(action.get());
+
+  llvm::outs() << "finished!\n";
+
+  /*
   auto calls = rosdiscover::api_call::RosApiCallFinder::find(tool);
   for (auto *call : calls) {
     call->print(llvm::outs());
     llvm::outs() << "\n\n";
   }
+  */
 
   return 0;
 }
