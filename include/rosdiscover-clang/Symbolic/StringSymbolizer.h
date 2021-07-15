a4425d6a4660593d05da482244170de58d230c87
ChrisTimperley <christimperley@googlemail.com>
2021-07-15 13:26:17 -0400

    moved more string symbolization code

--- include/rosdiscover-clang/Symbolic/StringSymbolizer.h
+++ include/rosdiscover-clang/Symbolic/StringSymbolizer.h
@@ -15,16 +15,50 @@ class StringSymbolizer {
 public:
   StringSymbolizer(clang::ASTContext &astContext) : astContext(astContext) {}
 
-  SymbolicString* symbolize(clang::Expr *nameExpr) {
-    return StringLiteral::create("PLACEHOLDER");
+  SymbolicString* symbolize(clang::Expr *expr) {
+    llvm::outs() << "symbolizing: ";
+    expr->dumpColor();
+    llvm::outs() << "\n";
+
+    if (clang::DeclRefExpr *declRefExpr = clang::dyn_cast<clang::DeclRefExpr>(expr)) {
+      return symbolize(declRefExpr);
+    } else if (clang::StringLiteral *literal = dyn_cast<clang::StringLiteral>(expr)) {
+      return symbolize(literal);
+    } else if (clang::ImplicitCastExpr *implicitCastExpr = dyn_cast<clang::ImplicitCastExpr>(expr)) {
+      return symbolize(implicitCastExpr);
+    } else if (clang::CXXConstructExpr *constructExpr = dyn_cast<clang::CXXConstructExpr>(expr)) {
+      return symbolize(constructExpr);
+    }
+
+    llvm::outs() << "unable to symbolize expression: treating as unknown\n";
+    return SymbolicUnknown::create();
   }
+
+private:
+  clang::ASTContext &astContext;
 
   SymbolicString* symbolize(clang::StringLiteral *literal) {
     return StringLiteral::create(literal->getString().str());
   }
 
-private:
-  clang::ASTContext &astContext;
+  SymbolicString* symbolize(clang::CXXConstructExpr *constructExpr) {
+    // does this call the std::string constructor?
+    // FIXME this is a bit hacky and may break when other libc++ versions are used
+    if (constructExpr->getConstructor()->getParent()->getQualifiedNameAsString() != "std::__cxx11::basic_string") {
+      return symbolize(constructExpr->getArg(0));
+    }
+
+    return SymbolicUnknown::create();
+  }
+
+  SymbolicString* symbolize(clang::ImplicitCastExpr *castExpr) {
+    // TODO check that we're dealing with strings or char[]
+    return symbolize(castExpr->getSubExpr());
+  }
+
+  SymbolicString* symbolize(clang::DeclRefExpr *nameExpr) {
+    return SymbolicUnknown::create();
+  }
 };
 
 } // rosdiscover::symbolic
