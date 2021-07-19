c7180de73b527a3465fb77c5eb845a1507f8ebce
ChrisTimperley <christimperley@googlemail.com>
2021-07-19 10:58:38 -0400

    retrieve name of parameter type

--- include/rosdiscover-clang/Symbolic/Function.h
+++ include/rosdiscover-clang/Symbolic/Function.h
@@ -9,10 +9,6 @@ namespace symbolic {
 // TODO for now, we don't record the arguments provided to the function call!
 class SymbolicFunction {
 public:
-  SymbolicFunction(std::string const &qualifiedName)
-    : qualifiedName(qualifiedName), body()
-  {}
-
   void print(llvm::raw_ostream &os) const {
     os << "function " << qualifiedName << " ";
     body.print(os);
@@ -26,9 +22,33 @@ public:
     return qualifiedName;
   }
 
+  static SymbolicFunction* create(
+      clang::FunctionDecl const *function
+  ) {
+    // TODO check whether this is the "main" function
+    auto numParams = function->getNumParams();
+    for (size_t paramIndex = 0; paramIndex < numParams; ++paramIndex) {
+      auto *param = function->getParamDecl(paramIndex);
+
+      // TODO does this have a default?
+
+      // get the name of the underlying unqualified type
+      auto typeName = param->getOriginalType().getUnqualifiedType().getAsString();
+      llvm::outs() << "Parameter type: " << typeName << "\n";
+    }
+
+
+    auto qualifiedName = function->getQualifiedNameAsString();
+    return new SymbolicFunction(qualifiedName);
+  }
+
 private:
   std::string qualifiedName;
   SymbolicCompound body;
+
+  SymbolicFunction(std::string const &qualifiedName)
+    : qualifiedName(qualifiedName), body()
+  {}
 };
 
 // TODO record symbolic function call arguments
