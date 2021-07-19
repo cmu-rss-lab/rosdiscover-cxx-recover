a4546ce6781ad56615cdde9556bcbb33b8935e2d
ChrisTimperley <christimperley@googlemail.com>
2021-07-19 12:09:57 -0400

    added getSymbolicType helper

--- include/rosdiscover-clang/Symbolic/Value.h
+++ include/rosdiscover-clang/Symbolic/Value.h
@@ -5,10 +5,31 @@
 namespace rosdiscover {
 namespace symbolic {
 
+enum class SymbolicValueType {
+  String,
+  Bool,
+  Integer,
+  Unsupported
+};
+
 class SymbolicValue {
 public:
   virtual ~SymbolicValue(){};
   virtual void print(llvm::raw_ostream &os) const = 0;
+
+  static SymbolicValueType getSymbolicType(clang::QualType clangType) {
+    clangType = clangType.getUnqualifiedType();
+    auto typeName = clangType.getAsString();
+    if (typeName == "std::string") {
+      return SymbolicValueType::String;
+    } else if (typeName == "bool") {
+      return SymbolicValueType::Bool;
+    } else if (typeName == "int") {
+      return SymbolicValueType::Integer;
+    } else {
+      return SymbolicValueType::Unsupported;
+    }
+  }
 }; // SymbolicValue
 
 class SymbolicString : public virtual SymbolicValue {};
