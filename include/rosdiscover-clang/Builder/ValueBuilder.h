6114b678e54595ea2390031cfca2f7c0405ee88e
ChrisTimperley <christimperley@googlemail.com>
2021-07-22 16:27:03 -0400

    added SymbolicVariable and VariableReference

--- include/rosdiscover-clang/Builder/ValueBuilder.h
+++ include/rosdiscover-clang/Builder/ValueBuilder.h
@@ -5,23 +5,28 @@
 #include "../Value/Bool.h"
 #include "../Value/String.h"
 #include "../Value/Value.h"
+#include "../Symbolic/Variable.h"
 
 namespace rosdiscover {
 namespace symbolic {
 
 class ValueBuilder {
 public:
-    std::unique_ptr<BoolLiteral> boolLiteral(bool literal) const {
-        return std::make_unique<BoolLiteral>(literal);
-    }
+  std::unique_ptr<BoolLiteral> boolLiteral(bool literal) const {
+    return std::make_unique<BoolLiteral>(literal);
+  }
 
-    std::unique_ptr<StringLiteral> stringLiteral(std::string const &string) const {
-        return std::make_unique<StringLiteral>(string);
-    }
+  std::unique_ptr<StringLiteral> stringLiteral(std::string const &string) const {
+    return std::make_unique<StringLiteral>(string);
+  }
 
-    std::unique_ptr<SymbolicUnknown> unknown() const {
-        return std::make_unique<SymbolicUnknown>();
-    }
+  std::unique_ptr<SymbolicUnknown> unknown() const {
+    return std::make_unique<SymbolicUnknown>();
+  }
+
+  std::unique_ptr<VariableReference> varRef(SymbolicVariable const *var) {
+    return std::make_unique<VariableReference>(var);
+  }
 };
 
 } // rosdiscover::symbolic
---
+++ include/rosdiscover-clang/Symbolic/Variable.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <string>
+
+#include "../Value/Value.h"
+
+namespace rosdiscover {
+namespace symbolic {
+
+class SymbolicVariable {
+public:
+  virtual ~SymbolicVariable();
+  virtual std::string getName() const = 0;
+  virtual SymbolicValueType getType() const;
+
+  std::string getTypeAsString() const {
+    return SymbolicValue::getSymbolicTypeAsString(getType());
+  }
+};
+
+class VariableReference : public virtual SymbolicValue {
+public:
+  VariableReference(SymbolicVariable const *variable) : variable(variable) {}
+  ~VariableReference(){}
+
+  SymbolicVariable const * getVariable() const {
+    return variable;
+  }
+
+  void print(llvm::raw_ostream &os) const override {
+    os << variable->getName();
+  }
+
+  nlohmann::json toJson() const override {
+    return {
+      {"kind", "variable-reference"},
+      {"variable", variable->getName()},
+      {"type", variable->getTypeAsString()}
+    };
+  }
+
+private:
+  SymbolicVariable const *variable;
+};
+
+
+} // rosdiscover::symbolic
+} // rosdiscover
