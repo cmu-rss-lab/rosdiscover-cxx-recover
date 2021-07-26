0f2c32ad5fd4352798d7a18c94392ebd94305b3f
ChrisTimperley <christimperley@googlemail.com>
2021-07-26 15:47:18 -0400

    removed redundant code

--- src/rosdiscover/RosDiscover.cpp
+++ src/rosdiscover/RosDiscover.cpp
@@ -26,17 +26,5 @@ int main(int argc, const char **argv) {
   auto program = ProgramSymbolizer::symbolize(optionsParser.getCompilations(), optionsParser.getSourcePathList());
   auto json = program->toJson();
   std::cout << std::setw(2) << json;
-
-  // TODO avoid the need to run things via ClangTool
-  // https://eli.thegreenplace.net/2012/06/08/basic-source-to-source-transformation-with-clang
-  // ClangTool tool(optionsParser.getCompilations(), optionsParser.getSourcePathList());
-
-  // TODO build and merge ASTs
-  // - raise an error if building or merging failed
-  // - buildASTs(&asts)
-
-  // build some function summaries
-  // auto action = rosdiscover::summary::SummaryBuilderAction::factory();
-  // tool.run(action.get());
   return 0;
 }
