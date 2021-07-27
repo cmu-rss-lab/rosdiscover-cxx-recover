fa5a086c1984883dd2faadabc163036006430035
ChrisTimperley <christimperley@googlemail.com>
2021-07-26 22:13:21 -0400

    updated example to use volumes to provide portable install!

--- example/navigation/run.sh
+++ example/navigation/run.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 set -eu
 
+VOLUME_ROSDISCOVER_NAME="rosdiscover-cxx-extract-opt"
+VOLUME_LLVM_NAME="rosdiscover-cxx-extract-llvm"
 SOURCE_FILES="/ros_ws/build src/turtlebot3_simulations/turtlebot3_gazebo/src/turtlebot3_drive.cpp"
 #SOURCE_FILES="/ros_ws/build src/turtlebot3_simulations/turtlebot3_fake/src/turtlebot3_fake.cpp"
 HERE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 DOCKER_DIR="${HERE_DIR}/../../docker"
 
-make -C "${DOCKER_DIR}"
+make -C "${DOCKER_DIR}" install
 
 pushd "${HERE_DIR}"
 docker build -t example .
 #docker run --rm -w /ros_ws -it example
 #docker run --rm -w /ros_ws -it example rosdiscover -p /ros_ws/build src/turtlebot3_simulations/turtlebot3_gazebo/src/turtlebot3_drive.cpp
-docker run --rm -w /ros_ws -it example rosdiscover -p /ros_ws/build ${SOURCE_FILES}
+docker run --rm \
+    -v "${VOLUME_ROSDISCOVER_NAME}:/opt/rosdiscover" \
+    -v "${VOLUME_LLVM_NAME}:/opt/llvm11" \
+    -w /ros_ws \
+    -it example \
+    rosdiscover -p /ros_ws/build ${SOURCE_FILES}
