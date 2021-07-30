90153912b789272435a30ccb7f68054dddea18ed
ChrisTimperley <christimperley@googlemail.com>
2021-07-29 21:07:20 -0400

    added prototype retrieval of topic types via ast matcher

--- include/rosdiscover-clang/ApiCall/Calls/SubscribeTopicCall.h
+++ include/rosdiscover-clang/ApiCall/Calls/SubscribeTopicCall.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <clang/AST/TemplateBase.h>
+
 #include "../RosApiCall.h"
 
 namespace rosdiscover {
@@ -17,6 +19,11 @@ public:
     return getCallExpr()->getArg(0);
   }
 
+  clang::CXXRecordDecl const * getTopicType() const override {
+    // TODO implement!
+    return nullptr;
+  }
+
   class Finder : public RosApiCall::Finder {
   public:
     Finder(std::vector<RosApiCall*> &found) : RosApiCall::Finder(found) {}
@@ -24,12 +31,24 @@ public:
     const clang::ast_matchers::StatementMatcher getPattern() override {
       using namespace clang::ast_matchers;
       return callExpr(
-        callee(cxxMethodDecl(hasName("subscribe"), ofClass(hasName("ros::NodeHandle"))))
+        callee(cxxMethodDecl(
+          hasTemplateArgument(0, templateArgument().bind("templateArg")),
+          hasName("subscribe"), ofClass(hasName("ros::NodeHandle"))))
       ).bind("call");
     }
 
   protected:
     RosApiCall* build(clang::ast_matchers::MatchFinder::MatchResult const &result) override {
+      llvm::outs() << "nice!\n";
+      auto qualType = result.Nodes.getNodeAs<clang::TemplateArgument>("templateArg")->getAsType().getNonReferenceType().getUnqualifiedType();
+      auto *recordType = clang::dyn_cast<clang::RecordType>(qualType.getTypePtr());
+      auto const *recordDecl = recordType->getDecl();
+      if (auto *specializationDecl = clang::dyn_cast<clang::ClassTemplateSpecializationDecl>(recordDecl)) {
+        recordDecl = specializationDecl->getSpecializedTemplate()->getTemplatedDecl();
+        llvm::outs() << "decl name: " << recordDecl->getQualifiedNameAsString() << "\n";
+      }
+      recordDecl->dump();
+      llvm::outs() << "\n";
       return new SubscribeTopicCall(result.Nodes.getNodeAs<clang::CallExpr>("call"));
     }
   };
