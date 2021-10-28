60acf79b3e746f1aabb5b5f92fa76cfc287a30e1
ChrisTimperley <christimperley@googlemail.com>
2021-10-28 15:54:16 -0400

    trim trailing slashes in format type names

--- include/rosdiscover-clang/Helper/FormatHelper.h
+++ include/rosdiscover-clang/Helper/FormatHelper.h
@@ -5,8 +5,14 @@
 namespace rosdiscover {
 
 std::string typeNameToFormatName(std::string typeName) {
+  // remove the trailing underscore, if there is any
+  if (typeName.back() == '_') {
+    typeName = typeName.substr(0, typeName.size() - 1);
+  }
+
   auto separatorPos = typeName.find("::");
   if (separatorPos == std::string::npos) {
+    llvm::outs() << "WARNING: unable to find :: separator in C++ type name for ROS format\n";
     return typeName;
   }
 
