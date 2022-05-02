fc93a7c8e4aff26ec083f574e4010e3e246977bc
Tobias Dürschmid <duerschmid@cmu.edu>
2022-05-02 13:21:33 -0400

    Add Publish Call class skeleton

---
+++ include/rosdiscover-clang/ApiCall/Calls/PublishCall.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <llvm/ADT/APInt.h>
+
+#include "../RosApiCall.h"
+
+namespace rosdiscover {
+namespace api_call {
+
+class PublishCall : public BareRosApiCall {
+public:
+  using BareRosApiCall::BareRosApiCall;
+
+  PublishCallKind const getKind() const override {
+    return RosApiCallKind::PublishCall;
+  }
+
+  clang::Expr const * getNameExpr() const override {
+    return getCallExpr()->getArg(2);
+  }
+
+  class Finder : public RosApiCall::Finder {
+  public:
+    Finder(std::vector<RosApiCall*> &found) : RosApiCall::Finder(found) {}
+
+    const clang::ast_matchers::StatementMatcher getPattern() override {
+      using namespace clang::ast_matchers;
+      return callExpr(
+        callee(functionDecl(hasName("ros::publish")))
+      ).bind("call");
+    }
+
+  protected:
+    RosApiCall* build(clang::ast_matchers::MatchFinder::MatchResult const &result) override {
+      auto *call = result.Nodes.getNodeAs<clang::CallExpr>("call");
+      return new RosInitCall(call);
+    }
+  };
+};
+
+} // rosdiscover::api_call
+} // rosdiscover
