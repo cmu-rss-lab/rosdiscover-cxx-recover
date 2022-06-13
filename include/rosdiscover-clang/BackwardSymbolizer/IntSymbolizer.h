feb775856bb5d2499c44b9e456265f527ae910c5
Tobias Dürschmid <duerschmid@cmu.edu>
2022-06-13 15:59:58 -0400

    Draft of using evaluateAsInt

---
+++ include/rosdiscover-clang/BackwardSymbolizer/IntSymbolizer.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+#include <clang/AST/Expr.h>
+#include <clang/AST/ExprCXX.h>
+#include <clang/AST/APValue.h>
+
+#include "../Builder/ValueBuilder.h"
+#include "../Value/String.h"
+#include "../Helper/FindDefVisitor.h"
+
+namespace rosdiscover {
+
+class IntSymbolizer {
+public:
+  IntSymbolizer(
+  )
+  : valueBuilder() {}
+
+  std::unique_ptr<SymbolicInteger> symbolize(clang::Expr *expr) {
+    expr = expr->IgnoreParenCasts();
+
+    llvm::outs() << "symbolizing: ";
+    expr->dump();
+    llvm::outs() << "\n";
+
+    if (clang::IntegerLiteral *literal = clang::dyn_cast<clang::IntegerLiteral>(expr)) {
+      return symbolize(literal);
+    } 
+
+    llvm::outs() << "unable to symbolize expression: treating as unknown\n";
+    return valueBuilder.unknown();
+  }
+
+private:
+  ValueBuilder valueBuilder;
+
+  std::unique_ptr<SymbolicInteger> symbolize(clang::IntegerLiteral *literal) {
+    return valueBuilder.integerLiteral(literal->getValue().getSExtValue());
+  }
+
+  std::unique_ptr<SymbolicInteger> symbolize(clang::APValue literal) {
+    return valueBuilder.integerLiteral(literal->getInt()->getSExtValue());
+  }
+};
+} // rosdiscover
