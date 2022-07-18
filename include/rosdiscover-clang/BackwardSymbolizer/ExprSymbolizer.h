f11596d9514c73091243ecbe5e97719e0023865b
Tobias Dürschmid <duerschmid@cmu.edu>
2022-07-18 14:17:48 -0400

    Symbolize negate expr

--- include/rosdiscover-clang/BackwardSymbolizer/ExprSymbolizer.h
+++ include/rosdiscover-clang/BackwardSymbolizer/ExprSymbolizer.h
@@ -30,7 +30,7 @@ public:
     floatSymbolizer()
   {}
 
-  std::unique_ptr<SymbolicExpr> symbolize(clang::Expr *expr) {
+  std::unique_ptr<SymbolicExpr> symbolize(const clang::Expr *expr) {
     if (expr == nullptr) {
       llvm::outs() << "ERROR! Symbolizing (expr): NULLPTR";
       return valueBuilder.unknown();
@@ -38,10 +38,11 @@ public:
 
     expr = expr->IgnoreParenCasts()->IgnoreImpCasts()->IgnoreCasts();
 
-    if (auto *binaryOp = clang::dyn_cast<clang::BinaryOperator>(expr)) {
-      return symbolizeBinaryOp(binaryOp);
+    if (auto *binOpExpr = clang::dyn_cast<clang::BinaryOperator>(expr)) {
+      return symbolizeBinaryOp(binOpExpr);
+    } else if (auto *unaryOpExpr = clang::dyn_cast<clang::UnaryOperator>(expr)) {
+      return symbolizeUnaryOp(unaryOpExpr);
     } 
-
 
     llvm::outs() << "symbolizing (expr): ";
     expr->dump();
@@ -51,15 +52,24 @@ public:
     return valueBuilder.unknown();
   }
 
-  std::unique_ptr<SymbolicExpr> symbolizeBinaryOp(clang::BinaryOperator *binOp) {
-    llvm::outs() << "unable to symbolize expression (expr): treating as unknown\n";
-    switch (binOp->getOpcode()) {
+  std::unique_ptr<SymbolicExpr> symbolizeBinaryOp(const clang::BinaryOperator *binOpExpr) {
+    switch (binOpExpr->getOpcode()) {
       case clang::BinaryOperator::Opcode::BO_LAnd: 
-        return std::make_unique<AndExpr>(symbolize(binOp->getLHS()), symbolize(binOp->getRHS()));
+        return std::make_unique<AndExpr>(symbolize(binOpExpr->getLHS()), symbolize(binOpExpr->getRHS()));
       case clang::BinaryOperator::Opcode::BO_LOr: 
-        return std::make_unique<OrExpr>(symbolize(binOp->getLHS()), symbolize(binOp->getRHS()));        
+        return std::make_unique<OrExpr>(symbolize(binOpExpr->getLHS()), symbolize(binOpExpr->getRHS()));        
       default: 
-        llvm::outs() << "Unsupported boolean operator: " << binOp->getOpcode() << " in expr: " << prettyPrint(binOp, astContext);
+        llvm::outs() << "Unsupported binar operator: " << binOpExpr->getOpcode() << " in expr: " << prettyPrint(binOpExpr, astContext);
+        return valueBuilder.unknown();
+    }
+  }
+
+  std::unique_ptr<SymbolicExpr> symbolizeUnaryOp(const clang::UnaryOperator *unaryOpExr) {
+    switch (unaryOpExr->getOpcode()) {
+      case clang::UnaryOperator::Opcode::UO_LNot:
+        return std::make_unique<NegateExpr>(symbolize(unaryOpExr->getSubExpr()));
+      default: 
+        llvm::outs() << "Unsupported unary operator: " << unaryOpExr->getOpcode() << " in expr: " << prettyPrint(unaryOpExr, astContext);
         return valueBuilder.unknown();
     }
   }
